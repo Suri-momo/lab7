3e7c228bd0e962037c09df23565951aa0c5b5f40
Shuyu <momobelly@gmail.com>
2016-02-18 21:41:50 +0000

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var projects = require('../projects.json');
 
 /*
- * GET home page.
- */
+* GET home page.
+*/
 
 exports.view = function(req, res){
-  	res.render('index', projects);
+
+  var random_num = Math.random();
+  console.log(random_num);
+
+  if (random_num > 0.5) {
+    projects.grid = false;
+    res.render('index', projects);
+  } else {
+    res.redirect('/grid');
+  }
+
+};
+
+exports.viewGrid = function(req, res){
+  projects.grid = true;
+  res.render('index', projects);
 };
